69044641a95d510590362a4b6db2afeae9674bd9
alire-alavi <alirezaalavirazavi@yahoo.com>
2023-09-01 13:51:14 +0330

    modularized

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,29 +12,9 @@ fn main() {
             process::exit(1);
         });
 
-    run(result);
-}
-
-fn run(config: Config) {
-    let file_content = fs::read_to_string(config.file_path)
-        .expect("Hmm there was a problem reading the file");
-
-    println!("file contains: \n {}", file_content);
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-   fn build(args: &[String]) -> Result<Config, &'static str> { 
-    if args.len() < 3 {
-        return Err("not enough arguments");
+    if let Err(e) = minigrep::run(result) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Ok(Config {query, file_path})
-   }
 }
+
