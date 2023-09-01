b0a09e6c1b6132e03f76df4dbed6c705dada749e
alire-alavi <alirezaalavirazavi@yahoo.com>
2023-09-01 23:33:18 +0330

    done ✅

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     
     let result = Config::build(&args)
         .unwrap_or_else(|err| {
-            print!("Problem Parsing arguments: {err}");
+            eprintln!("Problem Parsing arguments: {err}");
             process::exit(1);
         });
 
     if let Err(e) = minigrep::run(result) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
